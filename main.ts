2fafc371c9bc8821fe69d43a5eae38a03698ce0f
MrChunk <eloludo79@hotmail.fr>
2025-03-31 13:22:32 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onButtonPressed(Button.A, function () {
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 80)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 80)
+})
+input.onButtonPressed(Button.B, function () {
+    Kitronik_Move_Motor.stop()
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+})
